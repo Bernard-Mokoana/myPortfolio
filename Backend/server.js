b0767d4f4a2aefbef178da802be3e4064bd8283d
Bernard <bernarddira@gmail.com>
2025-02-09 12:45:37 +0200

    "Prepare Backend for deployment"

--- Backend/server.js
+++ Backend/server.js
@@ -3,11 +3,13 @@ import cors from "cors";
 import contactRoutes from "./src/routes/contactRoutes.js"
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
 app.use(cors());
 
 app.use("/api", contactRoutes);
 
-app.listen(5000, () => 
-    console.log(`Server is running on http://localhost:5000`)
+app.listen(PORT, () => 
+    console.log(`Server is running on port ${PORT}`)
 );
